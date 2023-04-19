ba52b0205405b5b4160f2ea8428ad209ef5f0e66
DevanshShukla <devansh1352000@gmail.com>
2023-04-20 00:27:45 +0530

    add some private fields of type String and int

--- src/main/java/com/decoder135/springbootjdbcdemo/model/Course.java
+++ src/main/java/com/decoder135/springbootjdbcdemo/model/Course.java
@@ -1,4 +1,13 @@
 package com.decoder135.springbootjdbcdemo.model;
 
 public class Course {
+
+    private int courseId;
+
+    private String title;
+
+    private String description;
+
+    private String link;
+
 }
