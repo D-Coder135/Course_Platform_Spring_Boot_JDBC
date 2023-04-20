9dd97964ad88b2f11b140f511b218df3970f28b7
DevanshShukla <devansh1352000@gmail.com>
2023-04-20 11:24:23 +0530

    implement the required methods inside the CourseJdbcDAO.java class

--- src/main/java/com/decoder135/springbootjdbcdemo/dao/CourseJdbcDAO.java
+++ src/main/java/com/decoder135/springbootjdbcdemo/dao/CourseJdbcDAO.java
@@ -2,5 +2,33 @@ package com.decoder135.springbootjdbcdemo.dao;
 
 import com.decoder135.springbootjdbcdemo.model.Course;
 
+import java.util.List;
+import java.util.Optional;
+
 public class CourseJdbcDAO implements DAO<Course> {
+
+    @Override
+    public List<Course> list() {
+        return null;
+    }
+
+    @Override
+    public void create(Course course) {
+
+    }
+
+    @Override
+    public Optional<Course> get(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(Course course, int id) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
