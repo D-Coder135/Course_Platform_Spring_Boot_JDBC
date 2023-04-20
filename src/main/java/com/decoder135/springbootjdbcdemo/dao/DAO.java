906ba8c4758d91cec2fd0da307d44c9c35fb0f52
DevanshShukla <devansh1352000@gmail.com>
2023-04-20 11:21:20 +0530

    define a method of type Optional get()

--- src/main/java/com/decoder135/springbootjdbcdemo/dao/DAO.java
+++ src/main/java/com/decoder135/springbootjdbcdemo/dao/DAO.java
@@ -1,11 +1,14 @@
 package com.decoder135.springbootjdbcdemo.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DAO<T> {
 
     List<T> list();
 
     void create(T t);
+
+    Optional<T> get(int id);
 
 }
