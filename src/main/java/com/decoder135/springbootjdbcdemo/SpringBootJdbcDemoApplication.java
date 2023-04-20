06bc1b368409b856f17571a201ffffb7e24a0141
DevanshShukla <devansh1352000@gmail.com>
2023-04-20 11:41:54 +0530

    call the forEach() loop on the courses list object

--- src/main/java/com/decoder135/springbootjdbcdemo/SpringBootJdbcDemoApplication.java
+++ src/main/java/com/decoder135/springbootjdbcdemo/SpringBootJdbcDemoApplication.java
@@ -21,6 +21,8 @@ public class SpringBootJdbcDemoApplication {
 
         System.out.println("\n All Courses -------------------- \n");
         List<Course> courses = dao.list();
+
+        courses.forEach(System.out::println);
     }
 
 }
