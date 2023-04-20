23bac03b565290742d00b4743333ea6683542397
DevanshShukla <devansh1352000@gmail.com>
2023-04-20 11:39:30 +0530

    add a private field of type DAO<Course>

--- src/main/java/com/decoder135/springbootjdbcdemo/SpringBootJdbcDemoApplication.java
+++ src/main/java/com/decoder135/springbootjdbcdemo/SpringBootJdbcDemoApplication.java
@@ -1,10 +1,14 @@
 package com.decoder135.springbootjdbcdemo;
 
+import com.decoder135.springbootjdbcdemo.dao.DAO;
+import com.decoder135.springbootjdbcdemo.model.Course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJdbcDemoApplication {
+
+    private DAO<Course> dao;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJdbcDemoApplication.class, args);
